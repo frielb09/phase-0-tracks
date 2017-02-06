92241bf541eeb235aa21fd87ab5ba32acbdd612f
Ben Friel <frielb09@live.com>
2017-02-06 10:51:31 -0500

    added final touches of pseudocode

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,34 +1,74 @@
- def random_number(number)
-   fibonacci = [1,0]
-   n = number - 2
-   p = 0
-   q = 1
- n.times do
-   num = p
-   p = q
-   q = num + q
-   fibonacci << q
-   end
-       return fibonacci
+
+### Method that takes an array of integers and an integer to search for. 
+### Returns the index of the item or nil if the integer is not present in array.
+
+grades = [88, 60, 79, 90, 80, 85]
+    
+
+ def locate_array(grades, num)
+  grade_new = []
+  found = false
+ 
+  grades.each do |item|
+    grade_new << item
+    if item == num
+      found = true
+      break
+    end
+  end
+  if found == true
+    return grade_new.length - 1
+  else
+    return nil
+  end
  end
  
- puts random_number(100)
  
+ p locate_array(grades, 100)
+ p locate_array(grades, 80)
 
- class Array
-  def find_each_index find
-    found, index, q = -1, -1, []
-    while found
-      found = self[index+1..-1].index(find)
-      if found
-        index = index + found + 1
-        q << index
+##### Fibonacci Method 
+
+def fib(number)
+   style_fibonacci = [1,0]
+   n = number - 2
+   a = 0
+   b = 1
+ n.times do
+   num = a
+   a = b
+   b = num + b
+   style_fibonacci << b
+   end
+       return style_fibonacci
+ end
+ 
+ p fib(6)
+
+ #### Bubble Sort 
+ ### Define method
+ ### Create a variable that equals the grade.  
+ ### Loop through and swap through the grades, till all are true.
+ ### Use if till true to move grade.size values in numeric order.
+ ### Use a break if to stop the loop when the grades are in numeric order. 
+ ### Print the grades and end 
+ def bubble_sort_grades(grade)
+  n = grade.size 
+
+  loop do 
+    swapped = false
+
+    (n-1).times do |i|
+      if grade[i] > grade[i+1]
+        grade[i], grade[i+1]=grade[i+1], grade[i]
+        swapped = true
       end
-      end
-     q
+    end
+      break if not swapped
   end
+  grade
 end
 
-arr = [2, 3, 5, 6, 6, 7, 8, 2, 3, 1, 8, 3]
-p arr.find_each_index 8
-p arr.find_each_index 5
+grades = [60, 73, 88, 90, 82, 86, 95, 80]
+p bubble_sort_grades(grades)
+
